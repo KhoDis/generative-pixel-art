79e4f73214d333e92f2b84b94e14b00eeffbf736
KhoDis <khodisdev@gmail.com>
2023-12-07 22:47:23 +0300

    style: fix primitives.ts formatting

--- src/renderer/primitives.ts
+++ src/renderer/primitives.ts
@@ -3,16 +3,19 @@ import { leaf, pixel } from "./builders.ts";
 
 export function rect(width: number, height: number, color: Color): Group {
   const pixels = [];
+
   for (let x = 0; x < width; x++) {
     for (let y = 0; y < height; y++) {
       pixels.push(pixel({ x, y }, color));
     }
   }
+
   return leaf(pixels);
 }
 
 export function circle(radius: number, color: Color): Group {
   const pixels = [];
+
   for (let x = -radius; x < radius; x++) {
     for (let y = -radius; y < radius; y++) {
       if (x * x + y * y < radius * radius) {
@@ -26,13 +29,16 @@ export function circle(radius: number, color: Color): Group {
 
 export function line(start: Point, end: Point, color: Color): Group {
   const pixels = [];
+
   const dx = end.x - start.x;
   const dy = end.y - start.y;
   const steps = Math.max(Math.abs(dx), Math.abs(dy));
+
   for (let i = 0; i < steps; i++) {
     const x = Math.round(start.x + (dx * i) / steps);
     const y = Math.round(start.y + (dy * i) / steps);
     pixels.push(pixel({ x, y }, color));
   }
+
   return leaf(pixels);
 }
