417d9444c776ba971acd931640e98383b0e3b482
KhoDis <khodisdev@gmail.com>
2023-12-16 01:42:41 +0300

    docs: add docs

--- src/renderer/transformers/flatten.ts
+++ src/renderer/transformers/flatten.ts
@@ -1,14 +1,24 @@
 import { Figure, Placement, Shape } from "../types.ts";
 import { place, shape } from "../builders.ts";
 
-export function flatten(group: Figure): Shape {
-  const { type } = group;
+/**
+ * Flattens a figure into a single shape.
+ *
+ * Recursively flattens nested groups.
+ *
+ * Each pixel is adjusted according to the anchors of the groups it is nested in.
+ *
+ * @param figure - The figure to be flattened. Can be a group or a shape.
+ * @returns The flattened shape.
+ */
+export function flatten(figure: Figure): Shape {
+  const { type } = figure;
 
   if (type === "shape") {
-    return group;
+    return figure;
   }
 
-  const { groups, anchor } = group;
+  const { groups, anchor } = figure;
 
   const result: Placement[] = [];
 
--- src/renderer/types.ts
+++ src/renderer/types.ts
@@ -1,10 +1,22 @@
 import { PixelMap } from "./classes/PixelMap.ts";
 
+/**
+ * Represents a point in a 2D space.
+ * @property x - The x coordinate.
+ * @property y - The y coordinate.
+ */
 export type Point = {
   x: number;
   y: number;
 };
 
+/**
+ * Represents a color using RGBA.
+ * @property r - The red component (0-255).
+ * @property g - The green component (0-255).
+ * @property b - The blue component (0-255).
+ * @property [a] - The alpha component for opacity (0-1). Optional.
+ */
 export type Color = {
   r: number;
   g: number;
@@ -12,22 +24,45 @@ export type Color = {
   a?: number;
 };
 
+/**
+ * Represents a figure which can be either a Group or a Shape.
+ */
 export type Figure = Group | Shape;
 
+/**
+ * Represents a group of figures.
+ * @property type - The type of the figure ("group").
+ * @property groups - The figures in the group.
+ * @property anchor - The anchor point of the group.
+ */
 export type Group = {
   type: "group";
   groups: Figure[];
   anchor: Point;
 };
 
+/**
+ * Represents a shape.
+ * @property type - The type of the figure ("shape").
+ * @property pixels - The pixels that make up the shape.
+ * @see PixelMap
+ */
 export type Shape = {
   type: "shape";
   pixels: PixelMap;
 };
 
+/**
+ * Represents a placement of a pixel at a certain position.
+ * @property position - The position of the pixel.
+ * @property pixel - The pixel to be placed.
+ */
 export type Placement = {
   position: Point;
   pixel: Pixel;
 };
 
+/**
+ * Represents a pixel which is essentially a color.
+ */
 export type Pixel = Color;
