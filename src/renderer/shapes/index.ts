b1c030f2c9b83f9c64720a440a87b0507e26fa53
KhoDis <khodisdev@gmail.com>
2024-01-01 17:14:57 +0300

    fix: add default to export

--- src/renderer/shapes/index.ts
+++ src/renderer/shapes/index.ts
@@ -4,4 +4,4 @@ import circle from "./circle.ts";
 import line from "./line.ts";
 import polygon from "./polygon.ts";
 
-export { pixel, rect, circle, line, polygon };
+export default { pixel, rect, circle, line, polygon };
