b0f9ac5bc04a0b9ebc776a9e7220d259e1e2e351
KhoDis <khodisdev@gmail.com>
2024-01-01 17:11:22 +0300

    refactor: update fill and outline

--- src/renderer/algorithms/outline.ts
+++ src/renderer/algorithms/outline.ts
@@ -1,29 +1,70 @@
 import { Color, Placement, Shape } from "../types.ts";
-import getNeighbors from "./utils/getNeighbors.ts";
+import {
+  getDiagonalNeighbors,
+  getStraightNeighbors,
+} from "./utils/getNeighbors.ts";
 import { shape } from "../builders.ts";
+
+export type OutlineMode = "none" | "all" | "corners";
 
 /**
  * Creates an outline around a shape.
  * @param what The shape to outline.
  * @param color The color of the outline.
- * @param diagonals Whether to include diagonals.
+ * @param mode Determines how diagonals are handled. Can be one of:
+ * - "none": No diagonals are included.
+ * - "all": All diagonals are included.
+ * - "corners": Only corner diagonals are included.
+ * @returns The outlined shape.
  */
 export default function outline(
   what: Shape,
   color: Color,
-  diagonals: boolean = false,
+  mode: OutlineMode = "corners",
 ): Shape {
-  const newPixels = what.pixels;
+  const pixels = what.pixels;
   const outlinePixels: Placement[] = [];
 
-  for (const { position } of newPixels) {
-    const neighbors = getNeighbors(position, diagonals);
-    for (const neighbor of neighbors) {
-      if (!newPixels.get(neighbor)) {
+  // Place vertical and horizontal outline pixels
+  for (const { position } of pixels) {
+    const straightNeighbors = getStraightNeighbors(position);
+    for (const neighbor of straightNeighbors) {
+      if (!pixels.has(neighbor)) {
         outlinePixels.push({ position: neighbor, pixel: color });
       }
     }
   }
 
-  return shape(...newPixels, ...outlinePixels);
+  // Place diagonal outline pixels
+  if (mode === "none") {
+    // nothing
+  } else if (mode === "all") {
+    for (const { position } of pixels) {
+      const diagonalNeighbors = getDiagonalNeighbors(position);
+      for (const neighbor of diagonalNeighbors) {
+        if (!pixels.has(neighbor)) {
+          outlinePixels.push({ position: neighbor, pixel: color });
+        }
+      }
+    }
+  } else if (mode === "corners") {
+    for (const { position } of pixels) {
+      const diagonalNeighbors = getDiagonalNeighbors(position);
+      diagonalNeighbors.forEach((neighbor, i) => {
+        // Check if target pixel is a corner
+        if (
+          !pixels.has(diagonalNeighbors[(i - 1 + 4) % 4]) && // smoothness check
+          !pixels.has(diagonalNeighbors[(i + 1) % 4]) && // smoothness check
+          !pixels.has(neighbor) // target placement
+        ) {
+          outlinePixels.push({
+            position: neighbor,
+            pixel: color,
+          });
+        }
+      });
+    }
+  }
+
+  return shape(...pixels, ...outlinePixels);
 }
