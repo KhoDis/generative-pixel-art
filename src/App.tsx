02b67da5ca1b6ccea5cf3a196250d2fbba1d85d0
KhoDis <khodisdev@gmail.com>
2024-01-11 00:40:35 +0300

    feat: add new rect constructors

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,21 @@ import KonvaRenderer from "./components/KonvaRenderer";
 import { Figure } from "./renderer/types";
 import shapes from "./renderer/modifiers/shapes";
 import colors from "./renderer/palettes/html.ts";
-import fill from "./renderer/modifiers/painting/fill.ts";
-import flatten from "./renderer/modifiers/transformers/flatten.ts";
-import outline from "./renderer/modifiers/painting/outline.ts";
-import move from "./renderer/modifiers/builders/move.ts";
-import rotate from "./renderer/modifiers/transformers/rotate.ts";
 import { combine, point } from "./renderer/factories";
+import reset from "./renderer/modifiers/builders/reset.ts";
+import { translate } from "./renderer/modifiers/transformers";
+import pixel from "./renderer/modifiers/shapes/pixel.ts";
 
-const triangle = shapes.polygon(
-  [point(0, 0), point(10, 0), point(0, 10)],
-  colors.red,
-  true,
-);
-
-const filledTriangle = fill(flatten(triangle), point(3, 3), colors.blue);
-
-const outlinedTriangle = outline(filledTriangle, colors.black);
-
-const rotatedTriangle = rotate(outlinedTriangle, point(5, 5), "180");
-
-const scene: Figure = combine(move(rotatedTriangle, point(10, 10)));
+const rect = shapes.rect.fromPoints(point(0, 0), point(8, 8), colors.red);
+const fig = reset(rect, point(1, 1));
+console.log(rect.pixels);
+console.log(fig.pixels);
+// move fig 10, 10
+const moved = translate(fig, point(10, 10));
+// draw one pixel at 9, 9
+const px = pixel(colors.blue, point(9, 9));
+// combine the two
+const scene: Figure = combine(moved, px);
 
 function App() {
   return (
