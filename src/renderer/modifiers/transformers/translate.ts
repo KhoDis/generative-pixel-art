3eae09a13b7919dc1e752b8d1b1e308d00850c4e
KhoDis <khodisdev@gmail.com>
2024-01-11 00:34:46 +0300

    refactor: rename shift to translate

--- src/renderer/modifiers/transformers/translate.ts
+++ src/renderer/modifiers/transformers/translate.ts
@@ -3,14 +3,14 @@ import { place } from "../../factories";
 import { shape } from "../builders";
 
 /**
- * Shifts a shape to a specified position.
+ * Translates a shape to a specified position.
  *
  * Updates the position of each pixel in the shape.
  * @param what - The shape to move.
  * @param [offset={ x: 0, y: 0 }] - The position to move the shape to.
  * @returns The shifted shape.
  */
-export default function shift(what: Shape, offset: Point): Shape {
+export default function translate(what: Shape, offset: Point): Shape {
   const pixels: Placement[] = [];
 
   for (const { position, pixel } of what.pixels) {
